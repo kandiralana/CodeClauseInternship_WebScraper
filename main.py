72bcc8f0a448eb47cf29c9a8afd51c0c2bba630f
Anastasiia Kandiral <kandiralana@gmail.com>
2024-02-15 10:10:46 +0100

    docstrings added

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from news_web_scraper import NewsScraper
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+
+def main():
+    """
+    Main function to run the news web scraper.
+
+    1. Creates a NewsScraper object for the specified URL.
+    2. Scrapes news information from the webpage.
+    3. Prints the processed news information.
+
+    :return: None
+    """
     url = 'https://globalnews.ca/world/'
 
     news_scraper = NewsScraper(url)
     news_scraper.scrape_news()
     news_scraper.print_news()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
--- news_web_scraper.py
+++ news_web_scraper.py
@@ -5,6 +5,12 @@ import time
 
 
 def get_page_to_scrap(p_url):
+    """
+    Create a session and retrieve the HTML content from the specified URL.
+
+    :param p_url: The URL to scrape.
+    :return: HTML content of the page.
+    """
     with requests.Session() as session:
         try:
             response_from_url = session.get(p_url)
@@ -18,8 +24,13 @@ def get_page_to_scrap(p_url):
     return response_from_url
 
 
-
 def find_news_headline(container):
+    """
+    Extract the news headline from the HTML container.
+
+    :param container: HTML container containing the news information.
+    :return: News headline if found, else None.
+    """
     headline_span = container.find('span', class_='c-posts__headlineText')
     if headline_span:
         return headline_span.get('title')
@@ -29,16 +40,33 @@ def find_news_headline(container):
 
 
 def find_news_date(container):
+    """
+    Extract the news date from the HTML container.
+
+    :param container: HTML container containing the news information.
+    :return: News date if found, else 'Date'.
+    """
     news_date = container.findAll('div', class_='c-posts__info')
     return news_date[1].text if news_date else 'Date'
 
 
 def find_news_link(container):
+    """
+    Extract the news link from the HTML container.
+
+    :param container: HTML container containing the news information.
+    :return: News link if found, else None.
+    """
     news_link = container.find('a', href=True)
     return news_link.get('href') if news_link else None
 
 
 def get_current_time():
+    """
+    Get the current time in a formatted string.
+
+    :return: Formatted current time.
+    """
     current_time = time.time()
     time_struct = time.gmtime(current_time)
     formatted_time = time.strftime("%x %X", time_struct)
@@ -47,12 +75,20 @@ def get_current_time():
 
 class NewsScraper:
     def __init__(self, page_url):
+        """
+        Initialize the NewsScraper object.
+
+        :param page_url: URL of the page to scrape.
+        """
         self.url = page_url
         self.page_to_scrape = get_page_to_scrap(page_url)
         self.soup = BeautifulSoup(self.page_to_scrape.text, 'html.parser')
         self.processed_news = {}
 
     def scrape_news(self):
+        """
+        Scrape news information from the HTML page and store it in the processed_news dictionary.
+        """
         news_containers = self.soup.find_all('li', class_='c-posts__item')
 
         for container in news_containers:
@@ -66,6 +102,9 @@ class NewsScraper:
                 self.processed_news[news_headline] = {'date': news_date, 'link': news_link}
 
     def print_news(self):
+        """
+        Print the processed news information.
+        """
         print(f"\nDownloaded: {get_current_time()}\n")
         for news_headline, news_info in self.processed_news.items():
             news_date = news_info.get('date')
